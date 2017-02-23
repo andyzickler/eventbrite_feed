3f6738ef984c21b0e38ff94945aa3c53d87e647f
Andy Zickler <andy@zickler.us>
2017-02-23 17:27:52 -0500

    set port from environment variable

--- eventbritefeed/__main__.py
+++ eventbritefeed/__main__.py
@@ -1,11 +1,11 @@
+import os
 from eventbritefeed.server import EventbriteFeedHTTPServer
 
 def run():
-    print('starting server...')
-    # Server settings
-    # Choose port 8080, for port 80, which is normally used for a http server, you need root access
-    server_address = ('127.0.0.1', 3000)
-    httpd = EventbriteFeedHTTPServer(server_address)
+    port = os.getenv('EBF_PORT', 3011)
+    address = os.getenv('EBF_ADDRESS', '127.0.0.1')
+    print('starting server... Listening on {}:{}'.format(address, port))
+    httpd = EventbriteFeedHTTPServer((address, port))
     print('running server...')
     httpd.serve_forever()
 
